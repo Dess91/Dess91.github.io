6e4df5b211c7ccdbc321d74e78a3a78f48ecd072
louison <louison.gilles@supdecreation.com>
2021-02-13 01:10:17 +0100

    asa
    a

--- main.js
+++ main.js
@@ -2,6 +2,5 @@
 document.querySelector("h1").onclick =() => {
    document.querySelector ("p").style.transform = "none"
    document.body.style.backgroundColor ="green"
-}
+   var getRamdomColor = () => "#" + Math.floor(Math.random() * }
 
-alert("Test")
